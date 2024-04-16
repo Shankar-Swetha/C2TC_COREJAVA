13ede79e8d54c311c6aabcb22fb2061c661d9526
sweth <sweth@172.16.178.18>
2024-04-16 08:16:40 +0530

    Task

---
+++ C38/src/com/tns/day01/CharDemo.java
@@ -0,0 +1,24 @@
+package com.tns.day01;
+
+public class CharDemo { 
+	
+	public static void main(String[] args) { 
+		
+		char ch = 's'; 
+		System.out.println(ch); 
+		
+		char ch1 = 23; 
+		System.out.println(ch1); 
+		
+		char var1 = '\u00A7'; 
+		System.out.println(var1); 
+		
+		char var2 = '\u20AC'; 
+		System.out.println(var2); 
+		
+		int a = 'g'; 
+		System.out.println(a); 
+	} 
+}
+
+
---
+++ C38/src/com/tns/day01/IdentifersDemo.java
@@ -0,0 +1,15 @@
+package com.tns.day01;
+
+public class IdentifersDemo { 
+	
+	public static void main(String[] args) { 
+		 
+		int $number = 20; 
+		System.out.println("value of the number variable is : "+ $number); 
+		
+		String studentName="Aniket";  
+		System.out.println("value is : "+ studentName); 
+	} 
+} 
+
+
